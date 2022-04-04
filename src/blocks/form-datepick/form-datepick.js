cb07add4418b798e7146a21a4c53363725da463c
Maxim <sheterons@gmail.com>
2022-04-04 17:43:59 +0300

    Сделал и адаптировал шапку страницы

---
+++ src/blocks/form-datepick/form-datepick.js
@@ -0,0 +1,1 @@
+import './form-datepick.scss'
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './common/styles.scss'
+
+require('./blocks/page/page.js')
+require('./blocks/header/header.js')
+require('./blocks/nav/nav.js')
+require('./blocks/button/button.js')
+
+require('./blocks/landing/landing.js')
+require('./blocks/booking-card/booking-card.js')
+
+require('./blocks/form/form.js')
+require('./blocks/form-title/form-title.js')
+require('./blocks/form-datepick/form-datepick.js')
+
+// function requireAll(requireContext) {
+//     return requireContext.keys().map(requireContext);
+// }
+
+// requireAll(require.context('./blocks', true, /^.*\.js$/));
