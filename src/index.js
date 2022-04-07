00d055139dcfcb8e7ee9449983e7cc8eda0df917
SectumImperial <sheterons@gmail.com>
2022-04-07 12:40:44 +0300

    Создан блок title, каточки брони был задан грид и общие стили позиционирования её элементов

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ require('./blocks/page/page.js')
 require('./blocks/header/header.js')
 require('./blocks/nav/nav.js')
 require('./blocks/button/button.js')
+require('./blocks/title/title.js')
 
 require('./blocks/landing/landing.js')
 require('./blocks/booking-card/booking-card.js')
 
 require('./blocks/form/form.js')
-require('./blocks/form-title/form-title.js')
 require('./blocks/input/input.js')
 require('./blocks/select/select.js')
