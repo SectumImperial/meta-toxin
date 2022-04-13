4694b2c05ad5677c939a9cd880fe5c14de1cbd96
SectumImperial <sheterons@gmail.com>
2022-04-13 21:00:58 +0300

    Удалены свойства объекта с классами для миксинов, где это возможно. DatePicker перенесён в блок input и добавляется в случае соответствующего свойства в опциях

--- src/blocks/datePicker/datePicker.js
+++
@@ -1,5 +0,0 @@
-import './datePicker.scss'
-import AirDatepicker from 'air-datepicker'
-import 'air-datepicker/air-datepicker.css'
-
-let dp = new AirDatepicker('#testDate')
--- src/blocks/input/input.js
+++ src/blocks/input/input.js
@@ -1,1 +1,9 @@
 import './input.scss'
+
+import AirDatepicker from 'air-datepicker'
+import 'air-datepicker/air-datepicker.css'
+
+const dateElems = document.querySelectorAll('.datepicker')
+dateElems.forEach((e) => {
+  let dp = new AirDatepicker(e)
+})
