4fab8548705540c98c237762ee420ffcdb3a8aa1
SectumImperial <sheterons@gmail.com>
2022-04-26 17:37:44 +0300

    Написал скрипт для рабоыт каленаря при кликах на разные инпуты

--- src/blocks/form/datepick.js
+++ src/blocks/form/datepick.js
@@ -6,15 +6,17 @@ import 'air-datepicker/air-datepicker.css'
 
 // const dp = new AirDatepicker(document.querySelector('#booking-out'))
 const container = document.querySelector('._datepick-js')
-let item = undefined,
-  firstItem = undefined,
-  secondItem = undefined
 
 if (container) {
+  let item, firstItem, secondItem, currentFeild
+  let formGroups = Array.from(container.querySelectorAll('.form__group'))
+
   let calConteiner = document.createElement('div')
   calConteiner.className = 'datepick_container'
   container.append(calConteiner)
-  let dp = new AirDatepicker('.datepick_container')
+  let dp = new AirDatepicker(calConteiner, {
+    range: true,
+  })
   dp.show()
 
   if (container.classList.contains('_datepick-2')) {
@@ -27,6 +29,33 @@ if (container) {
 
   container.addEventListener('click', ({ target }) => {
     if (target.closest('.datepick_container')) return
+
+    let targetContainer = target.closest('.form__group')
+    let sibling = [...formGroups.filter((e) => e !== targetContainer)][0]
+
+    // Клик по сосденему инпуту после клика на первый, календарь не закрывается
+    if (
+      !targetContainer.classList.contains('clicked') &&
+      sibling.classList.contains('clicked') &&
+      calConteiner.classList.contains('_active')
+    ) {
+      targetContainer.classList.toggle('clicked')
+      return
+    }
+
+    // Закрыть календарь и снять все clicked на элементах в случае повторонго клика по любому элементу при открытом календаре
+    if (
+      targetContainer.classList.contains('clicked') &&
+      sibling.classList.contains('clicked') &&
+      calConteiner.classList.contains('_active')
+    ) {
+      targetContainer.classList.remove('clicked')
+      sibling.classList.remove('clicked')
+      calConteiner.classList.remove('_active')
+      return
+    }
+
+    targetContainer.classList.toggle('clicked')
     calConteiner.classList.toggle('_active')
   })
 }
