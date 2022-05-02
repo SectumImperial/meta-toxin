a41d2356064cf932a1780c3ad51243dde85c5846
SectumImperial <sheterons@gmail.com>
2022-05-02 17:01:14 +0300

    Настроена работа выделения диапазона, кроме двух мелких деталей

--- src/blocks/form/datepick.js
+++ src/blocks/form/datepick.js
@@ -70,44 +70,96 @@ if (container) {
     let calContent = container.querySelector('.air-datepicker--content')
     let days = [...calContent.querySelectorAll('.-day-')]
 
-    calContent.addEventListener('click', ({ target }) => {
+    calContent.addEventListener('click', ({ targetClick }) => {
       // Удаление старых линий диапазона при выборе нового
       let startRangeLine = calContent.querySelector('.start-range')
       let endRangeLine = calContent.querySelector('.end-range')
-
-      let rangeFrom = calContent.querySelector('.-range-from-')
-      let rangeTo = calContent.querySelector('.-range-to-')
-
-      if (startRangeLine && endRangeLine) {
+      if (
+        startRangeLine &&
+        !startRangeLine.classList.contains('-selected-') &&
+        endRangeLine &&
+        !endRangeLine.classList.contains('-selected-')
+      ) {
         startRangeLine.classList.remove('start-range')
         endRangeLine.classList.remove('end-range')
       }
 
-      // Добавление новой линии во время движения
-      let hasRangeFrom = days.some((e) => {
-        return e.classList.contains('-range-from-')
-      })
+      let rangeFrom = calContent.querySelector('.-range-from-')
+      let rangeTo = calContent.querySelector('.-range-to-')
 
-      if (rangeTo && !rangeTo.classList.contains('-selected-')) {
-        calContent.addEventListener(
-          'mouseover',
-          ({ target, relatedTarget }) => {
-            let hasRangeTo = days.some((e) => {
-              return e.classList.contains('-range-to-')
-            })
+      // Переменная для пред. дня при движении мыши во время выделения диапазона
+      let prevDay
+      // Выделить диапазон при движеии мыши
+      calContent.addEventListener('mouseover', ({ target, relatedTarget }) => {
+        rangeFrom = calContent.querySelector('.-range-from-')
+        rangeTo = calContent.querySelector('.-range-to-')
 
-            if (hasRangeFrom && hasRangeTo) {
-              rangeFrom.classList.add('start-range')
-              relatedTarget.classList.remove('end-range')
-              target.classList.add('end-range')
-            }
+        // Назначить переменной пред. дня значение
+        if (relatedTarget && !relatedTarget.classList.contains('-days-')) {
+          prevDay = relatedTarget
+        }
+
+        // Добавить выделение диапазона при движении мыши
+        if (
+          target.classList.contains('-range-to-') &&
+          !target.classList.contains('-range-from-')
+        ) {
+          target.classList.add('end-range')
+          console.log('add class range')
+          if (prevDay) prevDay.classList.remove('end-range')
+        }
+
+        if (
+          target.classList.contains('-range-from-') &&
+          !target.classList.contains('-range-to-')
+        ) {
+          target.classList.add('start-range')
+          console.log('add class range')
+          if (prevDay) prevDay.classList.remove('start-range')
+        }
+
+        // Удаление выделения диапазона у элементов там, где это не нужно при быстром движении мыши или выход за контейнер.
+        let startRangeLines = [...calContent.querySelectorAll('.start-range')]
+        let endRangeLines = [...calContent.querySelectorAll('.end-range')]
+        startRangeLines.forEach((e) => {
+          if (
+            !e.classList.contains('-range-from-') &&
+            !e.classList.contains('-selected-')
+          ) {
+            e.classList.remove('start-range')
           }
-        )
-      }
+        })
 
-      if (rangeTo && rangeTo.classList.contains('-selected-')) {
-        rangeTo.classList.add('end-range')
-      }
+        endRangeLines.forEach((e) => {
+          if (
+            !e.classList.contains('-range-to-') &&
+            !e.classList.contains('-selected-')
+          ) {
+            e.classList.remove('end-range')
+          }
+        })
+
+        // Удалить выделение диапазона в случае возврата мыши к выбранной дате
+        if (
+          rangeFrom.classList.contains('-focus-') &&
+          rangeFrom.classList.contains('-range-to-') &&
+          rangeFrom.classList.contains('-range-from-') &&
+          rangeFrom.classList.contains('-selected-')
+        ) {
+          rangeFrom.classList.remove('end-range')
+          rangeFrom.classList.remove('start-range')
+          console.log('remove class range')
+        }
+
+        // Добавить класс выделения диапазона первому и последнему выбранному элементу
+        if (rangeFrom && rangeFrom.classList.contains('-selected-')) {
+          rangeFrom.classList.add('start-range')
+        }
+
+        if (rangeTo && rangeTo.classList.contains('-selected-')) {
+          rangeTo.classList.add('end-range')
+        }
+      })
     })
   } catch (err) {
     console.error(err)
