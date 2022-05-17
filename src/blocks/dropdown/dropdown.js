fc9d1f0beb286d089ee7b5655a6dfcc123baee58
SectumImperial <sheterons@gmail.com>
2022-05-17 21:34:33 +0300

    Добавил скрипт счётчика, подправил его стили

--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -4,5 +4,6 @@ let dropdown = document.querySelector('.dropdown')
 let dropdownContent = dropdown.querySelector('.dropdown__content')
 
 dropdown.addEventListener('click', (e) => {
+  if (!e.target.classList.contains('dropdown')) return
   dropdownContent.classList.toggle('_active')
 })
