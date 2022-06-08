47ee1192899423968d5c07d206836e15b8066fa3
SectumImperial <sheterons@gmail.com>
2022-06-08 21:05:11 +0300

    Сделал стартовую страницу для быстрого перехода по прочим страницам

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './common/styles.scss'
 
+require('./pages/start-page/start-page.js')
+
+require('./blocks/landing/landing.js')
+
 require('./blocks/page-layout/page-layout.js')
 require('./blocks/header/header.js')
 require('./blocks/footer/footer.js')
@@ -10,12 +14,9 @@ require('./blocks/title/title.js')
 require('./blocks/text-element/text-element.js')
 require('./blocks/social/social.js')
 
-require('./blocks/landing/landing.js')
 require('./blocks/booking-card/booking-card.js')
-
 require('./blocks/form/form.js')
 require('./blocks/datepick/datepick.js')
 require('./blocks/input/input.js')
 require('./blocks/select/select.js')
-
 require('./blocks/dropdown/dropdown.js')
