27e495cc4ee4371a63541e039244d3c958f0ae58
SectumImperial <sheterons@gmail.com>
2022-06-23 16:58:02 +0300

    Написал скрипт поведения слайдера

--- src/blocks/range-slider/range-slider.js
+++ src/blocks/range-slider/range-slider.js
@@ -1,47 +1,50 @@
 import './range-slider.scss'
 
 const slider = document.querySelector('._js-range-slider')
+const fieldMin = document.querySelector('.range-slider__amount_min')
+const fieldMax = document.querySelector('.range-slider__amount_max')
+const toggleOne = document.querySelector('.range-slider__input_min')
+const toggleTwo = document.querySelector('.range-slider__input_max')
+const rangeProgress = document.querySelector('.range-slider__progress')
+
+const MIN = 0
+const MAX = 15000
+
 if (slider) {
-  const progress = slider.querySelector('.range-slider__progress')
-  const minToggle = slider.querySelector('.range-slider__min ')
-  const maxToggle = slider.querySelector('.range-slider__max')
+  toggleOne.value = '5000'
+  toggleTwo.value = '10000'
+  fieldMin.value = toggleOne.value
+  fieldMax.value = toggleTwo.value
 
-  const toggles = document.querySelectorAll('.range-slider__toggle')
+  rangeProgress.style.left = (toggleOne.value / MAX) * 100 + '%'
+  rangeProgress.style.right = 100 - (toggleTwo.value / MAX) * 100 + '%'
 
-  function moveToggle(event, toggle) {
-    event.preventDefault() // предотвратить запуск выделения (действие браузера)
-
-    let shiftX = event.clientX - toggle.getBoundingClientRect().left
-
-    document.addEventListener('mousemove', onMouseMove)
-    document.addEventListener('mouseup', onMouseUp)
-
-    function onMouseMove(event) {
-      let newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left
-
-      // курсор вышел из слайдера => оставить бегунок в его границах.
-      if (newLeft < 0) {
-        newLeft = 0
-      }
-      let rightEdge = slider.offsetWidth - toggle.offsetWidth
-      if (newLeft > rightEdge) {
-        newLeft = rightEdge
-      }
-
-      toggle.style.left = newLeft + 'px'
-    }
-
-    function onMouseUp() {
-      document.removeEventListener('mouseup', onMouseUp)
-      document.removeEventListener('mousemove', onMouseMove)
-    }
+  function colorRange() {
+    rangeProgress.style.left = (toggleOne.value / MAX) * 100 + '%'
+    rangeProgress.style.right = 100 - (toggleTwo.value / MAX) * 100 + '%'
   }
 
-  //   Навесить обработчики события на toggle
-  toggles.forEach((toggle) => {
-    toggle.addEventListener('mousedown', (event) => moveToggle(event, toggle))
-    toggle.ondragstart = function () {
-      return false
+  toggleOne.addEventListener('input', (e) => {
+    if (Number(toggleTwo.value) - Number(toggleOne.value) <= MIN) {
+      toggleOne.value = Number(toggleTwo.value) - MIN
     }
+
+    fieldMin.value = toggleOne.value
+    colorRange()
+
+    if (Number(toggleOne.value) === MAX && Number(toggleTwo.value) === MAX) {
+      toggleTwo.style.display = 'none'
+    } else {
+      toggleTwo.style.display = 'block'
+    }
+  })
+
+  toggleTwo.addEventListener('input', (e) => {
+    if (Number(toggleTwo.value) - Number(toggleOne.value) <= MIN) {
+      toggleTwo.value = Number(toggleOne.value) + MIN
+    }
+
+    fieldMax.value = toggleTwo.value
+    colorRange()
   })
 }
