55e4f4c234ce3a19dcc9be9c4241436ebc2ec3ef
SectumImperial <sheterons@gmail.com>
2022-06-24 20:30:06 +0300

    переписал код dropdown в класс

--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -5,58 +5,60 @@ import isEqual from './helpers/isEqual'
 import cutString from './helpers/cutString'
 import DEFAULT_KEY from './constants.js'
 
-const dropdown = document.querySelector('.dropdown')
-if (dropdown) {
-  const dropdownInput = dropdown.querySelector('.dropdown__input')
-  const dropdownContent = dropdown.querySelector('.dropdown__content')
+class Dropdown {
+  constructor(element) {
+    this.dropdown = element
 
-  let btnClear = dropdown.querySelector('.dropdown__button_clear')
-  let btnAccept = dropdown.querySelector('.dropdown__button_accept')
-
-  btnClear.style.visibility = 'hidden'
-
-  let options
-  try {
-    options = JSON.parse(dropdownInput.dataset.inputoptions)
-  } catch (err) {
-    throw new ValidationError('Ошибка в чтении options', err)
+    try {
+      this.options = JSON.parse(this.dropdown.dataset.options)
+      this.init()
+    } catch (err) {
+      throw new ValidationError('Ошибка в чтении options', err)
+    }
   }
 
-  dropdownInput.addEventListener('click', (e) => {
-    dropdownContent.classList.toggle('_active')
-  })
-
-  // Каунтер
-  const counter = dropdown.querySelector('.dropdown__counter')
-
-  const btnDecrement = counter.querySelectorAll('.dropdown__btn_decrement')
-  const btnIncrement = counter.querySelectorAll('.dropdown__btn_increment')
-
-  function checkLimits(countEl) {
-    if (Number(countEl.value) > 999) countEl.value = 999
-    if (Number(countEl.value) < 0) countEl.value = 0
-  }
-
-  function checkBtnVisibility(container, selector) {
-    const [...counts] = container.querySelectorAll(selector)
-    const countValues = []
-    counts.forEach((e) => countValues.push(Number(e.value)))
-    const sum = countValues.reduce(
-      (previousValue, currentValue) => previousValue + currentValue,
-      0
+  init() {
+    this.dropdownInput = this.dropdown.querySelector('.dropdown__input')
+    this.dropdownContent = this.dropdown.querySelector('.dropdown__content')
+    this.btnClear = this.dropdown.querySelector('.dropdown__button_clear')
+    this.btnAccept = this.dropdown.querySelector('.dropdown__button_accept')
+    this.checkBtnVisibility(this.dropdown, '.dropdown__count')
+    this.counter = this.dropdown.querySelector('.dropdown__counter')
+    this.btnsDecrement = this.counter.querySelectorAll(
+      '.dropdown__btn_decrement'
+    )
+    this.btnsIncrement = this.counter.querySelectorAll(
+      '.dropdown__btn_increment'
     )
 
-    if (sum > 0) btnClear.style.visibility = 'visible'
-    if (sum === 0) btnClear.style.visibility = 'hidden'
+    this.addListeners()
   }
 
-  /**
-   * Превращает объект в Map
-   *
-   * @param {object} obj - массив объектов с переданными данными для создани Map
-   * @return Map
-   */
-  function createObjectMap(arrObj) {
+  performData(selector, data) {
+    this.wordsMap = this.createObjectMap(data)
+    this.countsMap = this.createCountsMap(selector)
+    this.synthMap = this.joinMap(this.wordsMap, this.countsMap)
+    this.string = this.createStrMap(this.synthMap)
+    this.addInputValue(this.dropdownInput, this.string)
+  }
+
+  addListeners() {
+    this.dropdownInput.addEventListener('click', (e) => {
+      this.dropdownContent.classList.toggle('_active')
+    })
+
+    this.btnsDecrement.forEach((item) =>
+      item.addEventListener('click', this.btnDecrement.bind(this))
+    )
+    this.btnsIncrement.forEach((item) =>
+      item.addEventListener('click', this.btnIncrement.bind(this))
+    )
+
+    this.btnClear.addEventListener('click', this.clear.bind(this))
+    this.btnAccept.addEventListener('click', this.accept.bind(this))
+  }
+
+  createObjectMap(arrObj) {
     let optMap = new Map()
     for (let obj of arrObj) {
       for (const [key, value] of Object.entries(obj)) {
@@ -67,15 +69,9 @@ if (dropdown) {
     return optMap
   }
 
-  /**
-   * Находит все элементы по селектору с element.value > 0 и создаёт Map, где key: data-attr, value: element.value
-   *
-   * @param {string} selector - строка селектора counter
-   * @return Map
-   */
-  function createCountsMap(selector) {
+  createCountsMap(selector) {
     const countMap = new Map()
-    const counts = [...document.querySelectorAll(selector)].filter(
+    const counts = [...this.dropdown.querySelectorAll(selector)].filter(
       (e) => Number(e.value) > 0
     )
     for (let count of counts) {
@@ -84,50 +80,16 @@ if (dropdown) {
       }
       countMap.set(count.dataset.item, Number(count.value))
     }
-
     return countMap
   }
 
-  /**
-   * Суммирует нужные значения counter и возвращает число.
-   *
-   * @param {Map} countsMap - Map counter
-   * @param indicator - признак, по которому определяются НЕ нужные числа counter
-   * @return {number} число
-   */
-  function sumCounts(countsMap, wordsMap, indicator) {
-    let arrKyes = []
-    for (let [key, value] of countsMap) {
-      if (key !== indicator && !wordsMap.has(key)) {
-        arrKyes.push(value)
-      }
-    }
-    let reusult = arrKyes.reduce((prev, curr) => Number(prev) + Number(curr), 0)
-    return reusult
-  }
-
-  function hasIsEqualKey(compareMap, arr) {
-    for (let [key] of compareMap) {
-      if (isEqual(key, arr)) return true
-    }
-
-    return false
-  }
-
-  /**
-   * Объединяет Map объекта слов и Map counter в новый Map, где key: массив слов опций,
-   * value: сумма element.value совпадающео атрибута
-   *
-   * @param {Map} wordsMap - Map слов
-   * @param {Map} countsMap - Map counter
-   */
-  function joinMap(wordsMap, countsMap) {
+  joinMap(wordsMap, countsMap) {
     const synthMap = new Map()
     for (let [key, value] of countsMap) {
       // Если в карте массива слов есть отдельный массив для ключа
       if (wordsMap.has(key)) {
         // Если создаваемая карта не имеет такого ключа
-        if (!hasIsEqualKey(synthMap, wordsMap.get(key))) {
+        if (!this.hasIsEqualKey(synthMap, wordsMap.get(key))) {
           synthMap.set(wordsMap.get(key), value)
         }
       }
@@ -137,7 +99,7 @@ if (dropdown) {
         // Если создаваемая карта уже имеет нужный массив слов
         if (synthMap.has(wordsMap.get(DEFAULT_KEY))) {
           // перебрать и суммировать подходящие ключи countsMap, после установить в качестве value
-          const value = sumCounts(countsMap, wordsMap, DEFAULT_KEY)
+          const value = this.sumCounts(countsMap, wordsMap, DEFAULT_KEY)
           synthMap.set(wordsMap.get(DEFAULT_KEY), value)
         }
 
@@ -151,13 +113,7 @@ if (dropdown) {
     return synthMap
   }
 
-  /**
-   * Прохиодит по Map и создёт строку согласно ключу и значению всего Map
-   *
-   * @param {Map} map - Map с массивами слов и значениями
-   * @return {[string]} Массив строк
-   */
-  function createStrMap(map) {
+  createStrMap(map) {
     let arrStrings = []
     for (let [key, value] of map) {
       arrStrings.push(`${value} ${chooseWord(value, key)}`)
@@ -167,105 +123,90 @@ if (dropdown) {
     return result
   }
 
-  function addInputValue(input, value) {
+  addInputValue(input, value) {
     input.value = cutString(value, 20)
   }
 
-  /**
-   * Обрабатывает данные counter и переданного объекта
-   *
-   * @param {string} selector - строка селектора counter
-   * @param {Array} data - массив объектов с переданными данными для создани строки
-   */
-  function performData(selector, data) {
-    const wordsMap = createObjectMap(data)
-    const countsMap = createCountsMap(selector)
-    const synthMap = joinMap(wordsMap, countsMap)
-    const string = createStrMap(synthMap)
-    addInputValue(dropdownInput, string)
+  hasIsEqualKey(compareMap, arr) {
+    for (let [key] of compareMap) {
+      if (isEqual(key, arr)) return true
+    }
+
+    return false
   }
 
-  btnDecrement.forEach((item) => {
-    item.addEventListener('click', (e) => {
-      e.preventDefault()
+  sumCounts(countsMap, wordsMap, indicator) {
+    let arrKyes = []
+    for (let [key, value] of countsMap) {
+      if (key !== indicator && !wordsMap.has(key)) {
+        arrKyes.push(value)
+      }
+    }
+    let reusult = arrKyes.reduce((prev, curr) => Number(prev) + Number(curr), 0)
+    return reusult
+  }
 
-      let container = e.target.closest('.dropdown__item')
-      let count = container.querySelector('.dropdown__count')
+  checkBtnVisibility(container, selector) {
+    const [...counts] = container.querySelectorAll(selector)
+    const countValues = []
+    counts.forEach((e) => countValues.push(Number(e.value)))
+    const sum = countValues.reduce(
+      (previousValue, currentValue) => previousValue + currentValue,
+      0
+    )
 
-      checkLimits(count)
+    if (sum > 0) this.btnClear.style.visibility = 'visible'
+    if (sum === 0) this.btnClear.style.visibility = 'hidden'
+  }
 
-      if (
-        Number(count.value) > 0 &&
-        e.target.classList.contains('dropdown__btn_disabled')
-      )
-        e.target.classList.remove('dropdown__btn_disabled')
-      if (e.target.classList.contains('dropdown__btn_disabled')) return
-      count.value = Number(count.value) - 1
-
-      if (Number(count.value) === 0)
-        e.target.classList.add('dropdown__btn_disabled')
-
-      performData('.dropdown__count', options)
-      checkBtnVisibility(dropdown, '.dropdown__count')
-    })
-  })
-
-  btnIncrement.forEach((item) => {
-    item.addEventListener('click', (e) => {
-      e.preventDefault()
-
-      let container = e.target.closest('.dropdown__item')
-      let count = container.querySelector('.dropdown__count')
-      let decrement = container.querySelector('.dropdown__btn_decrement')
-
-      checkLimits(count)
-
-      if (Number(count.value) === 999) return
-      count.value = Number(count.value) + 1
-
-      if (Number(count.value) > 0)
-        decrement.classList.remove('dropdown__btn_disabled')
-
-      performData('.dropdown__count', options)
-      checkBtnVisibility(dropdown, '.dropdown__count')
-    })
-  })
-
-  btnClear.addEventListener('click', (e) => {
+  btnDecrement(e) {
     e.preventDefault()
-    dropdownInput.value = ''
+    let container = e.target.closest('.dropdown__item')
+    let count = container.querySelector('.dropdown__count')
+    this.checkLimits(count)
+    if (
+      Number(count.value) > 0 &&
+      e.target.classList.contains('dropdown__btn_disabled')
+    )
+      e.target.classList.remove('dropdown__btn_disabled')
+    if (e.target.classList.contains('dropdown__btn_disabled')) return
+    count.value = Number(count.value) - 1
+    if (Number(count.value) === 0)
+      e.target.classList.add('dropdown__btn_disabled')
+    this.performData('.dropdown__count', this.options)
+    this.checkBtnVisibility(this.dropdown, '.dropdown__count')
+  }
 
-    const [...counts] = dropdown.querySelectorAll('.dropdown__count')
+  btnIncrement(e) {
+    e.preventDefault()
+    let container = e.target.closest('.dropdown__item')
+    let count = container.querySelector('.dropdown__count')
+    let decrement = container.querySelector('.dropdown__btn_decrement')
+    this.checkLimits(count)
+    if (Number(count.value) === 999) return
+    count.value = Number(count.value) + 1
+    if (Number(count.value) > 0)
+      decrement.classList.remove('dropdown__btn_disabled')
+    this.performData('.dropdown__count', this.options)
+    this.checkBtnVisibility(this.dropdown, '.dropdown__count')
+  }
+
+  checkLimits(countEl) {
+    if (Number(countEl.value) > 999) countEl.value = 999
+    if (Number(countEl.value) < 0) countEl.value = 0
+  }
+
+  clear(e) {
+    e.preventDefault()
+    this.dropdownInput.value = ''
+    const [...counts] = this.dropdown.querySelectorAll('.dropdown__count')
     counts.forEach((e) => (e.value = 0))
-  })
+  }
 
-  btnAccept.addEventListener('click', (e) => {
+  accept(e) {
     e.preventDefault()
-    dropdownContent.classList.remove('_active')
-  })
-
-  // Добавить значение из строки url, если они есть
-  const queryString = window.location.search
-  const urlParams = new URLSearchParams(queryString)
-  const dropdownUrlContent = urlParams.get('dropdown')
-
-  if (dropdownUrlContent) {
-    addInputValue(dropdownInput, dropdownUrlContent)
-
-    let params = window.location.search
-      .replace('?', '')
-      .split('&')
-      .reduce(function (p, e) {
-        let a = e.split('=')
-        p[decodeURIComponent(a[0])] = decodeURIComponent(a[1])
-        return p
-      }, {})
-
-    let allCounts = dropdown.querySelectorAll('.dropdown__count')
-    allCounts.forEach((e) => {
-      e.value = params[e.dataset.item]
-    })
-
-    checkBtnVisibility(dropdown, '.dropdown__count')
+    this.dropdownContent.classList.remove('_active')
   }
 }
+
+export default Dropdown
--- src/blocks/search-room/search-room.js
+++ src/blocks/search-room/search-room.js
@@ -1,1 +1,6 @@
 import './search-room.scss'
+
+import Dropdown from '../dropdown/dropdown'
+document.querySelectorAll('._js-dropdown').forEach((e) => {
+  new Dropdown(e)
+})
