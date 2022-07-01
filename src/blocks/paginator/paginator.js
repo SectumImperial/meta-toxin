935e5e09c2532d94e6160593cfadab079986af7a
SectumImperial <sheterons@gmail.com>
2022-07-01 19:41:40 +0300

    Paginator работает со значениями в середине всего списка страниц

--- src/blocks/paginator/paginator.js
+++ src/blocks/paginator/paginator.js
@@ -23,7 +23,8 @@ class Paginator {
     this.liClass = LI_CLASS
     this.textElement = this.paginator.querySelector('.paginator__text')
     this.count = COUNT_PAGE
-    this.currentPage = 1
+    this.currentPage = 6
+    this.startPage = 1
 
     try {
       this.options = JSON.parse(this.paginator.dataset.paginator)
@@ -38,23 +39,69 @@ class Paginator {
     this.pageCount = Math.ceil(this.allItems / this.itemsPerPage)
 
     this.createPaginator()
+    this.setCurrentPage()
     this.checkVisibilityBtn()
     this.addListeners()
     this.createText()
   }
 
   createPaginator() {
-    for (let i = this.currentPage; i <= this.pageCount; i++) {
-      let li = document.createElement('li')
-      li.className = this.liClass
-      if (i === 1) {
-        li.classList.add(ACTIVE_LI)
-        li.classList.add(ACTIVE_JS)
+    let end = Number(this.currentPage)
+    let start = Number(this.currentPage)
+    end++
+    start--
+
+    if (
+      this.currentPage > COUNT_PAGE &&
+      this.currentPage < this.pageCount - COUNT_PAGE
+    ) {
+      for (let i = start; i <= end; i++) {
+        // let li = document.createElement('li')
+        // li.className = this.liClass
+
+        // li.innerText = i
+        // li.dataset.number = i
+        // this.itemsPaginator.append(li)
+
+        let li = this.createLiElement(this.liClass, i)
+        this.itemsPaginator.append(li)
       }
-      li.innerText = i
-      li.dataset.number = i
+
+      //   Добавить точки
+      let li = this.createLiElement(this.liClass, '...', true)
       this.itemsPaginator.append(li)
+      this.itemsPaginator.prepend(li.cloneNode(true))
+
+      //   Добавить первую страницу
+      let startLi = this.createLiElement(this.liClass, this.startPage)
+      this.itemsPaginator.prepend(startLi)
+
+      // Добавить последнюю страницу
+      let lastLi = this.createLiElement(this.liClass, this.pageCount)
+      this.itemsPaginator.append(lastLi)
     }
+  }
+
+  /**
+   *
+   * @param {string} className
+   * @param {number or string} number
+   * @param {boolean} dot
+   */
+  createLiElement(className, content, dots = false) {
+    let el = document.createElement('li')
+    el.className = className
+    el.innerText = content
+    if (dots) {
+      el.dataset.dots = true
+      el.classList.add(`${this.liClass}_dots`)
+    } else {
+      el.dataset.number = content
+    }
+    return el
+  }
+  removePaginator() {
+    this.itemsPaginator.innerHTML = ''
   }
 
   addListeners() {
@@ -94,55 +141,51 @@ class Paginator {
 
   decrementPage() {
     if (this.currentPage !== 1) {
-      let current = this.itemsPaginator.querySelector(
-        `.paginator__item[data-number="${this.currentPage}"]`
-      )
-      current.classList.remove(ACTIVE_LI)
-      current.classList.remove(ACTIVE_JS)
+      this.removePage()
       this.currentPage--
-
-      let newPage = this.itemsPaginator.querySelector(
-        `.paginator__item[data-number="${this.currentPage}"]`
-      )
-      newPage.classList.add(ACTIVE_LI)
-      newPage.classList.add(ACTIVE_JS)
-
-      this.checkVisibilityBtn()
+      this.removePaginator()
+      this.createPaginator()
+      this.setCurrentPage()
     }
   }
 
   incrementPage() {
     if (this.currentPage !== this.pageCount) {
-      let current = this.itemsPaginator.querySelector(
-        `.paginator__item[data-number="${this.currentPage}"]`
-      )
-      current.classList.remove(ACTIVE_LI)
-      current.classList.remove(ACTIVE_JS)
+      this.removePage()
       this.currentPage++
-
-      let newPage = this.itemsPaginator.querySelector(
-        `.paginator__item[data-number="${this.currentPage}"]`
-      )
-      newPage.classList.add(ACTIVE_LI)
-      newPage.classList.add(ACTIVE_JS)
-
-      this.checkVisibilityBtn()
+      this.removePaginator()
+      this.createPaginator()
+      this.setCurrentPage()
     }
   }
 
   changePage({ target }) {
     if (!target.classList.contains('paginator__item')) return
+    if (target.dataset.dots) return
+    this.removePage()
+    this.currentPage = target.dataset.number
+    this.removePaginator()
+    this.createPaginator()
+    this.setCurrentPage()
+  }
+
+  setCurrentPage() {
+    let page = this.itemsPaginator.querySelector(
+      `.paginator__item[data-number="${this.currentPage}"]`
+    )
+
+    page.classList.add(ACTIVE_LI)
+    page.classList.add(ACTIVE_JS)
+
+    this.checkVisibilityBtn()
+  }
+
+  removePage() {
     let current = this.itemsPaginator.querySelector(
       `.paginator__item[data-number="${this.currentPage}"]`
     )
     current.classList.remove(ACTIVE_LI)
     current.classList.remove(ACTIVE_JS)
-
-    this.currentPage = target.dataset.number
-    target.classList.add(ACTIVE_LI)
-    target.classList.add(ACTIVE_JS)
-
-    this.checkVisibilityBtn()
   }
 }
 
