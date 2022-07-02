3b1f96491de001a4bfe02d1b33377d95b1d66b6a
SectumImperial <sheterons@gmail.com>
2022-07-02 19:20:40 +0300

    Адаптирвоал страницу поиска

--- src/blocks/search-room/search-room.js
+++ src/blocks/search-room/search-room.js
@@ -1,1 +1,14 @@
 import './search-room.scss'
+
+const filtersBtn = document.querySelector('.search-room__button')
+const filters = document.querySelector('.search-room__filters')
+filtersBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  filters.classList.toggle('_active')
+  if (filters.classList.contains('_active')) {
+    filtersBtn.querySelector('.button__content').innerText = 'Скрыть фильтры'
+  }
+  if (!filters.classList.contains('_active')) {
+    filtersBtn.querySelector('.button__content').innerText = 'Раскрыть фильтры'
+  }
+})
