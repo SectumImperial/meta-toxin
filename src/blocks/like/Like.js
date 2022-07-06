bd7772ad07e690c769dbc24e6885af1f3707f311
SectumImperial <sheterons@gmail.com>
2022-07-06 15:25:46 +0300

    Added behaviour of counting for the like class

--- src/blocks/like/Like.js
+++ src/blocks/like/Like.js
@@ -25,10 +25,30 @@ class Like {
   }
 
   toggleLike() {
+    if (this.button.classList.contains(LIKED_BUTTON)) {
+      this.decrementLike()
+    }
+
+    if (!this.button.classList.contains(LIKED_BUTTON)) {
+      this.incrementLike()
+    }
+
     this.button.classList.toggle(LIKED_BUTTON)
     this.heart.classList.toggle(LIKED__HEART)
     this.heart.classList.toggle(UNLIKED__HEART)
     this.count.classList.toggle(LIKED_COUNT)
+  }
+
+  decrementLike() {
+    let val = this.count.innerText
+    val--
+    this.count.innerText = val
+  }
+
+  incrementLike() {
+    let val = this.count.innerText
+    val++
+    this.count.innerText = val
   }
 }
 
