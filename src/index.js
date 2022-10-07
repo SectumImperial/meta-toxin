c405854d646cc1334891055f7c36bfe779f7a006
SectumImperial <sheterons@gmail.com>
2022-10-07 19:09:47 +0300

    fixed imports STRUCTURE #2

--- src/index.js
+++ src/index.js
@@ -1,58 +1,10 @@
 import './common/styles.scss';
 
-const pages = [
-  'start-page',
-  'error-page',
-  'ui-colors-type',
-  'ui-cards',
-  'ui-headers-footers',
-  'ui-form-elements',
-];
+const cache = {};
 
-const files = [
-  'registration',
-  'login',
-  'landing',
-  'search-room',
-  'room-details',
-  'header',
-  'footer',
-  'nav',
-  'button',
-  'title',
-  'text-element',
-  'social',
-  'booking-card',
-  'datepicker',
-  'input',
-  'dropdown',
-  'radio',
-  'toggle',
-  'range-slider',
-  'checkbox',
-  'checkbox-list',
-  'room-card',
-  'carousel',
-  'star-rating',
-  'paginator',
-  'room-data',
-  'canvas',
-  'review',
-  'like',
-  'rules',
-  'order-card',
-  'room-number',
-  'room-price',
-  'registration-card',
-  'login-card',
-  'ui-header',
-  'subscription-field',
-];
+function importAll(r) {
+  // eslint-disable-next-line no-return-assign
+  r.keys().forEach((key) => (cache[key] = r(key)));
+}
 
-// eslint-disable-next-line global-require, import/no-dynamic-require
-pages.forEach((e) => require(`./pages/${e}/index`));
-// eslint-disable-next-line global-require, import/no-dynamic-require
-files.forEach((e) => require(`./blocks/${e}/index`));
-// eslint-disable-next-line global-require, import/no-dynamic-require
-require('./layouts/page-layout/index');
-require('./layouts/ui-layout/index');
+importAll(require.context('../src/', true, /\.js$/));
