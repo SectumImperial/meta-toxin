d55a61e80f711a57b4a8eb3693748e36b6ca8a95
SectumImperial <sheterons@gmail.com>
2022-12-25 20:50:36 +0300

    fixed PP

--- src/pages/room-page/index.js
+++ src/pages/room-page/index.js
@@ -1,7 +1,7 @@
 import chooseWord from '../../helpers/chooseWord';
 import './room-page.scss';
 
-const reviewCount = document.querySelector('.room-details__reviews-count');
+const reviewCount = document.querySelector('.room-page__reviews-count');
 if (reviewCount) {
   const count = document.querySelectorAll('.review').length;
   reviewCount.innerText = `${count} ${chooseWord(count, [
