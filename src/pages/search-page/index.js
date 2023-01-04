106508b6aa581a96e1db67856b8c6f42e0bc60b9
SectumImperial <sheterons@gmail.com>
2023-01-04 21:34:41 +0300

    fixed word break UX #1

--- src/pages/search-page/index.js
+++ src/pages/search-page/index.js
@@ -6,11 +6,11 @@ const filters = document.querySelector('.search-room__filters');
 if (filtersBtn && filters) {
   filtersBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    filters.classList.toggle('_active');
-    if (filters.classList.contains('_active')) {
+    filters.classList.toggle('search-room__filters_active');
+    if (filters.classList.contains('search-room__filters_active')) {
       filtersBtn.querySelector('.button__content').innerText = 'Скрыть фильтры';
     }
-    if (!filters.classList.contains('_active')) {
+    if (!filters.classList.contains('search-room__filters_active')) {
       filtersBtn.querySelector('.button__content').innerText = 'Раскрыть фильтры';
     }
   });
