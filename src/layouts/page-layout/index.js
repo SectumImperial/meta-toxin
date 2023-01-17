36f8b55a5d337fbcf04d0514a0ea1dbd9f742af2
SectumImperial <sheterons@gmail.com>
2023-01-17 21:56:00 +0300

    Changed the usage of 100vh to clientHeight SCSS #3

--- src/layouts/page-layout/index.js
+++ src/layouts/page-layout/index.js
@@ -1,1 +1,5 @@
 import './page-layout.scss';
+
+if (document.querySelector('.page')) {
+  document.querySelector('.page').style.minHeight = `${document.querySelector('.page').clientHeight}px`;
+}
--- src/layouts/ui-layout/index.js
+++ src/layouts/ui-layout/index.js
@@ -1,1 +1,5 @@
 import './ui-layout.scss';
+
+if (document.querySelector('.ui-page')) {
+  document.querySelector('.ui-page').style.minHeight = `${document.querySelector('.ui-page').clientHeight}px`;
+}
