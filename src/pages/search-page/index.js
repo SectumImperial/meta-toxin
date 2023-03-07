181a11a0f384625a119352fd9154e1239dc137f1
SectumImperial <sheterons@gmail.com>
2023-03-07 21:34:26 +0300

    fixed button search page

--- src/pages/search-page/index.js
+++ src/pages/search-page/index.js
@@ -1,16 +1,16 @@
 import './search-page.scss';
 
-const filtersBtn = document.querySelector('.search-room__button');
-const filters = document.querySelector('.search-room__filters');
+const filtersBtn = document.querySelector('.search-page__button');
+const filters = document.querySelector('.search-page__filters');
 
 if (filtersBtn && filters) {
   filtersBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    filters.classList.toggle('search-room__filters_active');
-    if (filters.classList.contains('search-room__filters_active')) {
+    filters.classList.toggle('search-page__filters_active');
+    if (filters.classList.contains('search-page__filters_active')) {
       filtersBtn.querySelector('.button__content').innerText = 'Скрыть фильтры';
     }
-    if (!filters.classList.contains('search-room__filters_active')) {
+    if (!filters.classList.contains('search-page__filters_active')) {
       filtersBtn.querySelector('.button__content').innerText = 'Раскрыть фильтры';
     }
   });
